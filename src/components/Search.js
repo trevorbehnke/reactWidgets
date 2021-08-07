81ff3ff6176c5e359f516201fa7c79ee4cddcab1
trevor <me@trevorbehnke.com>
2021-08-06 21:48:35 -0700

    + search throttling & debouncing

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,12 +22,32 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
